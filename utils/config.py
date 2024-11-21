274bcfdd611f0eea7b0b1c15ab97f2bbaf6d33a1
ybokhari <younes.bokhari@outlook.com>
2024-11-21 17:14:32 +0100

    Add SDK Picsellia and get dataset

---
+++ utils/config.py
@@ -0,0 +1,7 @@
+import picsellia
+
+# Organization name
+organization_name = "Picsalex-MLOps"
+
+# Client initialization in the Trial environment
+client = Client(api_token="185e119a0205ca1a92bf65af09c0469eecca2500", organization_name=organization_name)
