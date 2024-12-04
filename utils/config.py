7166baf289fc367fdf1111f1fc95c2b4b11d6950
Arthur VILLARD <villardarthur@gmail.com>
2024-12-04 23:15:28 +0100

    picsellia config

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,14 @@
-import picsellia
+import picsellia as pc
 
-# Organization name
-organization_name = "Picsalex-MLOps"
+api_token = "185e119a0205ca1a92bf65af09c0469eecca2500"
+project_id = "01936421-2de9-7259-8a28-c059512eb2fb"
+dataset_id = "0193688e-aa8f-7cbe-9396-bec740a262d0"
 
-# Client initialization in the Trial environment
-client = Client(api_token="185e119a0205ca1a92bf65af09c0469eecca2500", organization_name=organization_name)
+client = pc.Client(
+	api_token=api_token,
+	organization_name="Picsalex-MLOps",
+	host="https://app.picsellia.com"
+)
+
+project = client.get_project_by_id("01936421-2de9-7259-8a28-c059512eb2fb")
+dataset: pc.DatasetVersion = client.get_dataset_version_by_id("0193688e-aa8f-7cbe-9396-bec740a262d0")
